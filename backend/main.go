f667bd0aef38471775f87473d700410b309aff32
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2024-11-24 18:43:07 +0700

    change go

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -37,7 +38,14 @@ type Project struct {
 var db *sql.DB
 
 func setupDB() *sql.DB {
-	connStr := "user=postgres password=392001 host=103.127.134.234 port=5432 dbname=postgres sslmode=disable"
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	var err error
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
