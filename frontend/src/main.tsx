8e3db291117d685b6f0460b7a35a7d6d3b66aa2e
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2025-01-12 10:32:38 +0700

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const initializeMatomo = () => {
+  const _paq = (window._paq = window._paq || []);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function () {
+    const u = 'https://matomo.izzudd.in/';
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    const d = document,
+      g = d.createElement('script'),
+      s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode?.insertBefore(g, s);
+  })();
+};
+
+initializeMatomo();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
